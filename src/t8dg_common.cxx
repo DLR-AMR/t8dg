43f164774c518a7f7c27f94c59b25b21badf07d4
Sven Goldberg <sven.goldberg@dlr.de>
2022-11-08 16:07:15 +0100

    Fix indentation

--- src/t8dg_common.cxx
+++ src/t8dg_common.cxx
@@ -52,12 +52,12 @@ t8dg_common_initial_cond_fn (int initial_cond_arg)
     return t8dg_mptrac_box_indicator_fn;
   case (19):
     return t8dg_williamson_etal_cosine_bell_fn;
-  case(20):
+  case (20):
     return t8dg_smooth_indicator3D_4Spheres_fn;
-  case(21):
+  case (21):
     return t8dg_smooth_indicator3D_3Spheres_above_below_fn;
-  case(22):
-    return t8dg_smooth_indicator3D_bottom_fn; 
+  case (22):
+    return t8dg_smooth_indicator3D_bottom_fn;
   default:
     return NULL;
   }
@@ -96,7 +96,7 @@ t8dg_scalar2d_angle (const double x[3], const double t, void *fn_data)
 double
 t8dg_scalar3d_cos_product (const double x[3], const double t, void *fn_data)
 {
-  t8dg_scalar3d_cos_product_data_t *cos_data = (t8dg_scalar3d_cos_product_data_t*) fn_data;
+  t8dg_scalar3d_cos_product_data_t *cos_data = (t8dg_scalar3d_cos_product_data_t *) fn_data;
   int                 dimension = cos_data->dim;
   double              diffusion_coefficient = cos_data->diffusion_coefficient;
   return exp (-diffusion_coefficient * dimension * 4 * M_PI * M_PI * t) * cos (2 * M_PI * x[0]) * cos (2 * M_PI * x[1]) * cos (2 * M_PI *
@@ -296,8 +296,8 @@ t8dg_smooth_indicator2Dfn (const double x[3], const double t, void *fn_data)
 double
 t8dg_smooth_indicator3Dfn (const double x[3], const double t, void *fn_data)
 {
-  double              radius = 1./(2 * 360); // 0.5 deg
-  double              smoothing_factor = 50; // 2.5 deg
+  double              radius = 1. / (2 * 360);  // 0.5 deg
+  double              smoothing_factor = 50;    // 2.5 deg
   double              center[3] = { 0.5, 0.5, 0.5 };
   double              dist = t8_vec_dist (x, center);
   if (dist < radius)
@@ -311,9 +311,9 @@ t8dg_smooth_indicator3Dfn (const double x[3], const double t, void *fn_data)
 double
 t8dg_smooth_indicator3D_bottom_fn (const double x[3], const double t, void *fn_data)
 {
-  double              radius = 1./(2 * 360);
+  double              radius = 1. / (2 * 360);
   double              smoothing_factor = 50;
-  double              center[3] = { 0.5, 0.5, 0.05};
+  double              center[3] = { 0.5, 0.5, 0.05 };
   double              dist = t8_vec_dist (x, center);
   if (dist < radius)
     return 1;
@@ -326,23 +326,22 @@ t8dg_smooth_indicator3D_bottom_fn (const double x[3], const double t, void *fn_d
 double
 t8dg_smooth_indicator3D_4Spheres_fn (const double x[3], const double t, void *fn_data)
 {
-  double              radius = 1./(2 * 360); // 0.5 deg
-  double              smoothing_factor = 50; // 2.5 deg
+  double              radius = 1. / (2 * 360);  // 0.5 deg
+  double              smoothing_factor = 50;    // 2.5 deg
 
   double              center[3] = { 0, 0.5, 0.5 };
   /* Array for x coordinates of different center for spheres */
   /* Center x=0 and x=1 coincide because of periodic boundaries */
-  double 	      center_x[5] = { 0, 0.25, 0.5, 0.75, 1 };
-  int                 center_x_len = sizeof(center_x)/sizeof(double);
+  double              center_x[5] = { 0, 0.25, 0.5, 0.75, 1 };
+  int                 center_x_len = sizeof (center_x) / sizeof (double);
 
   double              dist;
   double              dist_min = t8_vec_dist (x, center);
 
-  for (int idx=0; idx<center_x_len; idx++)
-  {
+  for (int idx = 0; idx < center_x_len; idx++) {
     center[0] = center_x[idx];
     dist = t8_vec_dist (x, center);
-    
+
     if (dist < dist_min)
       dist_min = dist;
 
@@ -360,20 +359,19 @@ t8dg_smooth_indicator3D_4Spheres_fn (const double x[3], const double t, void *fn
 double
 t8dg_smooth_indicator3D_3Spheres_above_below_fn (const double x[3], const double t, void *fn_data)
 {
-  double              radius = 1./(2 * 360); // 0.5 deg
-  double              smoothing_factor = 50; // 2.5 deg
+  double              radius = 1. / (2 * 360);  // 0.5 deg
+  double              smoothing_factor = 50;    // 2.5 deg
 
   double              center[3] = { 0.5, 0.25, 0.5 };
   /* Array for x coordinates of different center for spheres */
   /* Center x=0 and x=1 coincide because of periodic boundaries */
   double              center_y[3] = { 0.25, 0.5, 0.75 };
-  int                 center_y_len = sizeof(center_y)/sizeof(double);
+  int                 center_y_len = sizeof (center_y) / sizeof (double);
 
   double              dist;
   double              dist_min = t8_vec_dist (x, center);
 
-  for (int idx=0; idx<center_y_len; idx++)
-  {
+  for (int idx = 0; idx < center_y_len; idx++) {
     center[1] = center_y[idx];
     dist = t8_vec_dist (x, center);
 
@@ -397,57 +395,56 @@ t8dg_williamson_etal_cosine_bell_fn (const double x[3], const double t, void *fn
   /* Transformed test initial condition by Williamson et al., 1992; Test Case 1, p. 217 */
   /* Original version was made for 2D SWE  */
   /* Idea to transform to 3D:
-   *	- Transform z coordinate of cube \in [0,1] to altitude via interpolation
-   *	- To do so, transform min and max pressure to max and min altitude
-   *	- Build up an 'ellipse' in z direction shrank by a (arbitrary) factor
+   *    - Transform z coordinate of cube \in [0,1] to altitude via interpolation
+   *    - To do so, transform min and max pressure to max and min altitude
+   *    - Build up an 'ellipse' in z direction shrank by a (arbitrary) factor
    * */
-
 
   /* Earth constants  */
   const double        a = 6371220;
   const double        g = 9.80616;
-  const double        radius = a/3; 
-  const double 	      shrinking_factor_z = radius;//*0.05;
+  const double        radius = a / 3;
+  const double        shrinking_factor_z = radius;      //*0.05;
   /* Initial values  */
 
 //  const double        center[3] = {1.5*M_PI, 0, 40000}; /* last value needs to be set/thought of */
   const int           h_0 = 1000;
-  double              x_globe[3] = {0, 0, 0}; /* needs to be set, transform input x */
+  double              x_globe[3] = { 0, 0, 0 }; /* needs to be set, transform input x */
 
   /* Transform deg to rad within interpolation */
-  x_globe[0] = ((x[0]-0.5)*360.)*M_PI/180.;
-  x_globe[1] = ((x[1]-0.5)*180.)*M_PI/180.;
+  x_globe[0] = ((x[0] - 0.5) * 360.) * M_PI / 180.;
+  x_globe[1] = ((x[1] - 0.5) * 180.) * M_PI / 180.;
 
   /* Found in (arbitrary) output file on JUWELS */
   /* For calculation/interpolation of altitude max and min value Pressure levels: 1013.25, 878.364 ... 0.191952 hPa */
-  double	      pressure_max = 1031.25;
-  double 	      pressure_min = 0.191952;
-  double 	      altitude_min_in_m = Z (pressure_max) * 1000.;
+  double              pressure_max = 1031.25;
+  double              pressure_min = 0.191952;
+  double              altitude_min_in_m = Z (pressure_max) * 1000.;
   double              altitude_max_in_m = Z (pressure_min) * 1000.;
-  
+
   /* Used to place center at 0.5  */
-  double	      altitude_middle_in_m = Z ( (pressure_max+pressure_min)*0.5) * 1000.; 
+  double              altitude_middle_in_m = Z ((pressure_max + pressure_min) * 0.5) * 1000.;
   //(altitude_max_in_m + altitude_min_in_m) * 0.5;
 
-  /* x=1.5*M_PI and z=40000 before ; shifted to (0.5,0.5,0.5) on cube*/
-  const double        center[3] = {0., 0., altitude_middle_in_m}; /* last value needs to be set/thought of */
+  /* x=1.5*M_PI and z=40000 before ; shifted to (0.5,0.5,0.5) on cube */
+  const double        center[3] = { 0., 0., altitude_middle_in_m };     /* last value needs to be set/thought of */
 
   /* Great circle distance between input (transformed to globe) and center */
-  double              r = a * acos (sin (center[1]) * sin (x_globe[1]) + cos (center[1]) * cos (x_globe[1]) * cos (center[0]-x_globe[0]));
+  double              r = a * acos (sin (center[1]) * sin (x_globe[1]) + cos (center[1]) * cos (x_globe[1]) * cos (center[0] - x_globe[0]));
 
-  /* Convert altitude in cube to x_globe[2] in m via scaling with pressure_min and pressure_max*/
-  x_globe[2] = Z ( (1-x[2]) * pressure_max + x[2] * pressure_min ) * 1000.;
+  /* Convert altitude in cube to x_globe[2] in m via scaling with pressure_min and pressure_max */
+  x_globe[2] = Z ((1 - x[2]) * pressure_max + x[2] * pressure_min) * 1000.;
 
-  double              z_dist = fabs (x_globe[2]-center[2]);
+  double              z_dist = fabs (x_globe[2] - center[2]);
 
   /* Value of initial function; depending on x_globe and its great circle distance */
   double              concentration = 0;
 
   /* Ellipse around center to controll expansion in z direction; dist_z=radius overshoots the cube */
-  double              dist_pyth = sqrt (r*r + shrinking_factor_z*z_dist*z_dist);
-  
-  if ( dist_pyth <= radius )
-    concentration = (h_0*0.5) * (1 + cos (M_PI*dist_pyth/radius));
+  double              dist_pyth = sqrt (r * r + shrinking_factor_z * z_dist * z_dist);
+
+  if (dist_pyth <= radius)
+    concentration = (h_0 * 0.5) * (1 + cos (M_PI * dist_pyth / radius));
 
   return concentration;
 }
